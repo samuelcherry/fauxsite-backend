82799b1756154b86a55fa6c1041a6ecc4c5dd7d9
samuelcherry <samuelcherry92@gmail.com>
2026-01-25 14:37:42 -0700

    using connection string

--- server/index.ts
+++ server/index.ts
@@ -6,12 +6,9 @@ const {Pool} = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
+	connectionString: process.env.DB_CONNECTIONSTRING,
 	host: process.env.DB_HOST,
-	port: Number(process.env.DB_PORT),
-	database: process.env.DB_NAME,
-	user: process.env.DB_USER,
-	password: process.env.DB_password,
-	ssl: process.env.DB_SSL === 'true'
+	family: 4,
 });
 
 async function testConnection() {
