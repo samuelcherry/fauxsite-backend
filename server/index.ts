4a1ce9579a044721919b2080f4e953ee24849e94
samuelcherry <samuelcherry92@gmail.com>
2026-01-30 14:28:12 -0700

    fixed cors orgin

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 
 require('dotenv').config();
-app.use(cors(*))
+app.use(cors({ origin: '*' }))
 
 const pool = new Pool({
   connectionString: process.env.DB_DIRECTCONNECT,
