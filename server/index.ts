24b70a2a956e0c07bb349ef57d5343b058f8e7a8
samuelcherry <samuelcherry92@gmail.com>
2026-04-08 21:40:07 -0600

    added pool connect

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 const pool = new Pool({
-  connectionString: process.env.DB_DIRECTCONNECT,
+  connectionString: process.env.DB_POOLCONNECT,
   ssl: { rejectUnauthorized: false },
 });
 
